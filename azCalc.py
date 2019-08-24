72cbdb4656af718f0e1f05914ae630523aa7f54a
Daniel Huseman <huseman@tamu.edu>
2019-08-24 17:30:59 -0500

    Add files via upload

--- azCalc.py
+++ azCalc.py
@@ -3,13 +3,24 @@
 #8/20/2019
 #
 #Input two coordinates and program returns azimuth from point
-#A to Point B.
+#A to Point B, and displays an arrow pointing in the direction
+#of that azimuth.
 #
-#To Do: Generate plan view image (bitmap?) of ship with heading equal to 
-#calculated azimuth.
+#To Do: Build a GUI for the program
 
 import math
+from PIL import Image
+#import matplotlib
+#import numpy
 
+def boatPic(az):
+	#PIL rotates images counterclockwise, thus requiring the 
+	#input azimuth to be negative to achieve a clockwise rotation
+	azForPil = az * -1
+	pic = Image.open("C:\\Users\\Sonny\\Documents\\Python\\arrow.jpg")
+	rotated = pic.rotate(azForPil)
+	rotated.show()	
+	
 def azimuth(coordinates):
 	dX = coordinates[1][0]-coordinates[0][0]
 	dY = coordinates[1][1]-coordinates[0][1]
@@ -20,10 +31,10 @@ def azimuth(coordinates):
 	return([az, range])
 	
 def getCoordinates():
-	#coord1 = input(str("Input coordinate 1 in format x,y:"))
-	#coord2 = input(str("Input coordinate 2 in format x,y:"))
-	coord1 = '0,0'
-	coord2 = '-1.0112,1.929'	
+	coord1 = input(str("Input coordinate 1 in format x,y:"))
+	coord2 = input(str("Input coordinate 2 in format x,y:"))
+	#coord1 = '0,0'
+	#coord2 = '1,1'	
 	
 	coord1 = coord1.split(',')
 	coord1list = [float(coord1[0]),float(coord1[1])]
@@ -38,5 +49,6 @@ def main():
 	az_range = azimuth(coordinates)
 	print("Azimuth: ", az_range[0])
 	print("Range:   ", az_range[1])
+	boatPic(az_range[0])
 	
 main()
